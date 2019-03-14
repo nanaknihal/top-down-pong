f5a4aba24ced739890f15e902229b210340c47ca
Nanak Nihal Khalsa <nkhalsa@vassar.edu>
2019-03-13 21:10:12 -0700

    forgot to add until now. rudimentary pong is up with simple bug intentionally not fixed because it's funny. bug is visible when  AI wins

---
+++ pong.js
@@ -0,0 +1,270 @@
+jsPsych.plugins["pong"] = (function() {
+  var plugin = {};
+
+  plugin.info = {
+    name: 'pong',
+    parameters: {
+      /*levelNumber: {
+        type:jsPsych.plugins.parameterType.INT
+      },
+      ballSpeed: {
+        type: jsPsych.plugins.parameterType.INT,
+        defualt: 0.04
+      },
+      numRegularBalls: {
+        type: jsPsych.plugins.parameterType.INT,
+        defualt: 2
+      },
+      numExplodingBalls: {
+        type: jsPsych.plugins.parameterType.INT,
+        defualt: 2
+      }*/
+    }
+  }
+
+  plugin.trial = function(display_element, trial) {
+    var par = trial
+    //var w=par.gameWidth, h=par.gameHeight;
+    display_element.innerHTML =
+    "<div id='gameContainer' style='position: absolute; top: 50%; left: 50%; margin-right:50%; transform: translate(-50%, -50%); height: " + gameHeight + "px; width: " + gameWidth + "px; vertical-align: middle'>" +
+    //"<!-background image:--><img src='robomb-pngs/floor.png' height='" + h + "' width='" + w + "' style='position:absolute; margin:auto; z-index:-100'></img>" +
+    "<!--main canvas where game happens:-->" +
+    "<canvas id='mainCanvas' height='" + gameHeight + "' width = '" + gameWidth + "'></canvas>"
+    +
+    "<!--overlay canvas that doesn't need to be refreshed constantly:-->" +
+    "<canvas id='overlay' style='position:absolute; left: 0; top: 0; z-index:4' height='" + gameHeight + "' width = '" + gameWidth + "'></canvas>"
+
+    function Ball(x,y){
+      //x,y is top-left corner of rectangle representing this ball. size is the length of all the sides of this rectangle (it's a square)
+      this.x = x
+      this.y = y
+      this.size = 20
+
+      this.xVelocity = 5
+      this.yVelocity = 5
+
+      this.color = "white";
+    }
+
+    function Model(difficulty){
+      //human paddle
+      this.hPaddle = new HumanPaddle(gameHeight/2, 10, 100)
+      //ai paddle
+      this.aiPaddle = new AIPaddle(gameHeight/2, 10, 100)
+      this.ball = new Ball(gameWidth/2, gameHeight/2)
+      this.checkForCollisions = function(){
+        //if it collides with AI paddle
+        if(this.ball.x <= this.aiPaddle.width && this.ball.y >= this.aiPaddle.y && this.ball.y+this.ball.size <= this.aiPaddle.y+this.aiPaddle.length){
+          this.ball.x = this.aiPaddle.width
+          this.handleCollision(this.ball, this.aiPaddle)
+          //if it collides with left wall
+        } else if(this.ball.x <= 0){
+          this.ball.x = 0//reset it to 0 in case it went past so it doesn't collide weirdly
+          this.handleCollision(this.ball, null, "left")
+          //if it collides with human paddle
+        } else if(this.ball.x+this.ball.size >= gameWidth-this.hPaddle.width && this.ball.y >= this.hPaddle.y && this.ball.y+this.ball.size <= this.hPaddle.y+this.hPaddle.length){
+          this.ball.x = gameWidth-this.hPaddle.width-this.ball.size
+          this.handleCollision(this.ball, this.hPaddle)
+          //with right wall
+        } else if(this.ball.x+this.ball.size >= gameWidth){
+          this.ball.x = gameWidth-this.ball.size
+          this.handleCollision(this.ball, null, "right")
+          //top wall
+        } else if(this.ball.y <=0){
+          this.ball.y=0
+          this.handleCollision(this.ball, null, "top")
+          //bottom
+        } else if(this.ball.y + this.ball.size >= gameHeight){
+          this.ball.y=gameHeight-this.ball.size
+          this.handleCollision(this.ball, null, "bottom")
+        }
+      }
+      this.handleCollision = function(ball, paddle, wall){
+        if(wall == "top" || wall == "bottom"){
+          ball.yVelocity *=-1
+        } else if(paddle != null){
+          ball.xVelocity *=-1
+        } else if(wall == "left"){
+          ball.xVelocity*=-1
+          controller.humanWon();
+        } else if(wall == "right"){
+          ball.xvelocity *=-1
+          controller.aiWon();
+        }
+      }
+      this.update = function(){
+        this.checkForCollisions();
+        var b = this.ball
+        b.x+=b.xVelocity;
+        b.y+=b.yVelocity;
+          }
+
+    }
+
+    function View(){
+      this.mainctx = document.getElementById("mainCanvas").getContext("2d");
+      this.drawBorders = function(){
+        var mctx = this.mainctx
+        //draw borders
+        mctx.beginPath()
+        mctx.fillStyle = "white"
+        mctx.rect(0,0, 1, gameHeight)
+        mctx.rect(0,0, gameWidth, 1)
+        mctx.rect(0, gameHeight-1, gameWidth, 1)
+        mctx.rect(gameWidth-1, 1, 1, gameHeight)
+        mctx.fill()
+        mctx.textAlign = "center"
+        mctx.font = "54px Courier New"
+        mctx.fillText('PONG', gameWidth/2, 56)
+        mctx.closePath()
+
+      }
+      this.drawBall = function(){
+        var mctx = this.mainctx
+        var b = model.ball
+        mctx.beginPath()
+        mctx.fillStyle = b.color
+        mctx.rect(b.x, b.y, b.size, b.size)
+        mctx.fill()
+        mctx.closePath()
+        console.log(b)
+      }
+      this.drawHumanPaddle = function(){
+        var mctx = this.mainctx
+        var p = model.hPaddle
+        mctx.beginPath()
+        mctx.fillStyle = "#"+((1<<24)*Math.random()|0).toString(16) //random color, from ZPiDER's answer at https://stackoverflow.com/questions/1484506/random-color-generator
+        mctx.rect(gameWidth-p.width, p.y, p.width, p.length)
+        mctx.fill()
+        mctx.closePath()
+      }
+      this.drawAIPaddle = function(){
+        var mctx = this.mainctx
+        var p = model.aiPaddle
+        mctx.beginPath()
+        mctx.fillStyle = "#"+((1<<24)*Math.random()|0).toString(16) //random color, from ZPiDER's answer at https://stackoverflow.com/questions/1484506/random-color-generator
+        mctx.rect(0, p.y, p.width, p.length)
+        mctx.fill()
+        mctx.closePath()
+      }
+      this.update = function(){
+        var m = this.mainctx
+        m.clearRect(0,0,gameWidth, gameHeight)
+        this.drawBorders()
+        this.drawBall()
+        this.drawHumanPaddle()
+        this.drawAIPaddle()
+      }
+
+
+    }
+    function Controller(){
+      //when the mouse is moved, tell the human paddle to change position
+      document.addEventListener("mousemove", function(event){
+        var gameRect = document.getElementById("gameContainer").getBoundingClientRect()
+        var mouseYInGameCoord = event.clientY - gameRect.top
+        //0 is upper boundary for paddle position
+        if(mouseYInGameCoord < 0){mouseYInGameCoord = 0}
+        //gameHeight-paddle.length is lower boundary
+        if(mouseYInGameCoord + model.hPaddle.length > gameHeight){mouseYInGameCoord = gameHeight - model.hPaddle.length}
+        //move the human paddle
+        model.hPaddle.updatePosition(mouseYInGameCoord)
+
+      })
+
+      //create a custom event to move the paddle. to be consistent, ai and human paddles alike should be moved by events
+      //there could be multiple events on a setInterval so the motion is more variable, not just linear
+      document.addEventListener("aiMove", function(event){model.aiPaddle.updatePosition(event.moveToY)}, false)
+
+      this.AI = {
+        paddle: model.aiPaddle,
+        ball:model.ball,
+        //function to determine where the AI should move to (the point where the ball is set to intersect the left wall)
+        getIntersection: function(){
+          var ballWallDistance = this.ball.x //aipaddle is on left (x=0)
+          //intersection of ball and left wall will be a point in a triangle from the ball, to the point where it intersects the left wall, to the point (ball.x, 0)
+          //the ratio of the left side of triangle to the bottom side of triangle = the ratio of ball.yVelocity to ball.xVelocity
+          var leftToBottomRatio = this.ball.yVelocity/this.ball.xVelocity
+          var leftSideLength = ballWallDistance*leftToBottomRatio
+          var intersectionYCoord = this.ball.y+leftSideLength
+          return intersectionYCoord
+        },
+        meetBall: /*given the ball's position and velocity (included in the ball object), move the paddle so it will eventually hit the ball:*/function(ball){
+            var moveEvent = new Event("aiMove")
+            var nextY = controller.AI.calculateNextY();
+            moveEvent.moveToY = nextY
+            document.dispatchEvent(moveEvent)
+        },
+
+        nextYToMoveTo: model.aiPaddle.y,
+        calculateNextY: function(){
+          //make paddle movement smooth; gradually move to intersection point (and maybe make mistakes?)
+          var y = this.paddle.y + (this.ball.y - this.paddle.y)^4
+          //0 is upper boundary for paddle position
+          if(y < 0){y = 0}
+          //gameHeight-paddle.length is lower boundary
+          if(y + model.hPaddle.length > gameHeight){y = gameHeight - model.hPaddle.length}
+
+          return y
+        }
+
+
+      }
+
+      this.humanWon = function(){
+        alert("You beat this AI...but can you beat the robot apocolypse?")
+      }
+
+      this.aiWon = function(){
+        alert("You suck")
+      }
+    }
+
+    function HumanPaddle(yCoord, width, length){
+      this.width = width
+      this.length = length
+
+      this.y = yCoord
+
+      this.updatePosition = function(newY){
+        this.y = newY
+      }
+
+    }
+
+    function AIPaddle(yCoord, width, length){
+      this.width = width
+      this.length = length
+
+      this.y = yCoord
+
+      this.updatePosition = function(newY){
+        this.y = newY
+        //alert('s')
+      }
+
+    }
+
+    function updateGame(currentTime){
+      //curLevel.curTime = currentTime
+      //if(!stopping){
+      window.requestAnimationFrame(function(){
+          model.update()
+          view.update()
+          controller.AI.meetBall()
+          window.requestAnimationFrame(updateGame)
+      })
+      //}
+    }
+
+    function beginGame(){
+      updateGame()
+    }
+    var model = new Model();
+    var view = new View();
+    var controller = new Controller();
+    beginGame();
+  }
+
+  return plugin;
+})();
